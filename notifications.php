39bcfbd5d8ad997e821003c75f96bbd31737ed13
olls <faircliff.oliver@gmail.com>
2013-03-15 18:41:03 +0000

    Got rid of pi day msg

--- notifications.php
+++ notifications.php
@@ -9,6 +9,6 @@
 	$notificationNum = numNotifications($mysqli, $currentUser);
 ?>
 		<form method="post" action="<?=keepUrl()?>">
-			<input type='submit' name='viewNotifications' value='<? echo $notificationsToggle; if ($notificationNum > 0) { echo " ({$notificationNum})"; } ?>'>
+			<input type="submit" name="viewNotifications" value='<? echo $notificationsToggle; if ($notificationNum > 0) { echo " ({$notificationNum})"; } ?>'>
 			<input type="text" name="viewProfile" value="<?=$topProfileToggle?>" hidden="yes">
 		</form>
