9eb71bbf1f85c5a325c6beeef49d75ed54fbb000
olls <faircliff.oliver@gmail.com>
2013-03-15 22:14:37 +0000

    Fixed conflict with post.php and notifications

--- post.php
+++ post.php
@@ -2,17 +2,19 @@
 	include('inc.php');
 	include('postProcess.php');
 	
-	if (!isset($_GET['pid'])) { header ('location: index.php'); }
+	if (!isset($_GET['pid'])) {
+		header ('location: index.php');
+	}
 	
 	$singlePost = True;
 	
 	$pid = htmlspecialchars($_GET['pid'], ENT_QUOTES);
 	
 	$query = "SELECT * FROM `posts` WHERE `pid`='{$pid}'";
-	$result = query_DB($mysqli, $query);
+	$postResult = query_DB($mysqli, $query);
 	
-	if (mysqli_num_rows($result) != 0) {
-		$row = mysqli_fetch_assoc($result);
+	if (mysqli_num_rows($postResult) != 0) {
+		$row = mysqli_fetch_assoc($postResult);
 		
 		$profileData = userData($row['uid'], $mysqli, 'uid');
 		$postProfileData = $profileData;
@@ -24,32 +26,33 @@
 
 <html>
 	<head>
-		<title>Post <? if (mysqli_num_rows($result) != 0) { echo $row['pid']; } ?></title>
+		<title>Post <? if (mysqli_num_rows($postResult) != 0) { echo $row['pid']; } ?></title>
 		<? include('head.php'); ?>
 	</head>
 	<body>
 		<? include('header.php'); ?>
 		<div id="main">
 <?
-			if (mysqli_num_rows($result) != 0) {
+		if (mysqli_num_rows($postResult) != 0) {
 ?>
 			<h2><a href="user.php?username=<?=$profileData['username']?>"><?=$name?></a></h2>
 <?
-				}
-					include('login/loginForm.php');
-					include('searchResults.php');
-				
-				if (mysqli_num_rows($result) != 0) {
-					
-					if ($loggedIn) {
-						$uid = $currentUser['uid'];
-						include('submitComment.php');
-					}
-					
-					include 'displayPosts.php';
-				} else {
-					echo $postNotExist;
-				}
+		}
+		
+		include('login/loginForm.php');
+		include('searchResults.php');
+		
+		if (mysqli_num_rows($postResult) != 0) {
+			
+			if ($loggedIn) {
+				$uid = $currentUser['uid'];
+				include('submitComment.php');
+			}
+			
+			include 'displayPosts.php';
+		} else {
+			echo $postNotExist;
+		}
 ?>
 		</div>
 <?
