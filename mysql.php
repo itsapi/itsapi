e349dc9a14277d1d4b6c9c41ed91ce0fae0da428
grit96 <geraintwhite@gmail.com>
2013-03-24 20:14:11 +0000

    removed mysql.php

--- mysql.php
+++
@@ -1,8 +0,0 @@
-<?
-	function queryError($mysqli){
-		return 'MySQLi query failed: (' . mysqli_errno($mysqli) . ') ' . mysqli_error($mysqli);
-	}
-	$mysqli = mysqli_connect('localhost', 'root', 'miranda96', 'main');
-	if (mysqli_connect_errno($mysqli)) {
-		printf("Connect failed: %s\n", mysqli_connect_error());
-	}
