23e9553791d8d5e4f5f3367595eb4df882f34ad7
grit96 <geraintwhite@gmail.com>
2013-04-10 22:11:53 +0100

    Fixed delete users script

--- clearUsers.php
+++ clearUsers.php
@@ -2,4 +2,4 @@
 	include 'inc.php';
 	$timeDiff = time() - $verifyTimeOut;
 	if ($timeDiff < 0) { $timeDiff = 0; }
-	$result = query_DB($mysqli, "DELTE FROM users WHERE lastActivity<'{$timeDiff}' AND verify");
+	$result = query_DB($mysqli, "DELETE FROM users WHERE lastActivity<'{$timeDiff}' AND verify");
