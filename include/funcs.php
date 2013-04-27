4b368e50b99774412f56edf60edde62e02ebea59
grit96 <geraintwhite@gmail.com>
2013-04-27 22:58:00 +0100

    fixed gallery links parsing

--- include/funcs.php
+++ include/funcs.php
@@ -422,6 +422,10 @@
 						$output .= '<iframe width="560" height="350" src="http://www.youtube.com/embed/' . $youtubeWatchCode . '?wmode=direct" frameborder="0" allowfullscreen></iframe>';
 					} elseif (preg_match('{\.[0-9]{1,3}}', $imgFile) || isset($validImg[$imgFile]) || stripos($completeUrl, "{$GLOBALS['fileNames']['viewPhoto']}?iid=")) {
 						$output .= '<a href=' . $completeUrl . $target . '<img src=' . $completeUrl . '></a>';
+					} elseif (stripos($completeUrl, "{$GLOBALS['fileNames']['photo']}?iid=")) {
+						$completeUrl = explode($GLOBALS['fileNames']['photo'], $completeUrl);
+						$completeUrl = $completeUrl[0] . $GLOBALS['fileNames']['viewPhoto'] . $completeUrl[1];
+						$output .= '<a href=' . $completeUrl . $target . '<img src=' . $completeUrl . '></a>';
 					} else {
 						$output .= '<a href=' . $completeUrl . $target . "$domain$port$path" . '</a>';
 					}
