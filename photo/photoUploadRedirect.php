eb0b5b48bdefc6fbddee947924626a637b2b2155
grit96 <geraintwhite@gmail.com>
2013-04-27 18:12:55 +0100

    make thumbnail on photo upload

--- photo/photoUploadRedirect.php
+++
@@ -1,9 +0,0 @@
-<html>
-	<head>
-		<title>Photo Successfully Uploaded!</title>
-		<script>
-			alert('Your photo was successfully uploaded :D')
-			window.location.replace('<?=$fileNames['photo']?>?iid=<?=htmlspecialchars($_GET['iid'], ENT_QUOTES)?>');
-		</script>
-	</head>
-</html>
