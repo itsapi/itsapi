1300721cd11c915c1a485d07e307b4b1a6281e8d
grit96 <geraintwhite@gmail.com>
2013-04-27 21:28:03 +0100

    Notification label now hides when equal to zero

--- viewNotifications.php
+++ viewNotifications.php
@@ -10,9 +10,9 @@
 		} else {
 			echo("\t\t<ul id=\"notificationsBar\">\n");
 			while ($notification = mysqli_fetch_assoc($result)) {
-				echo("\t\t\t<li><a href=\"http://{$notification['link']}\">{$notification['title']}</a> <span class=\"date\">" . date('d/m/y G:i', $notification['date']) . "</span><form action=\"javascript: formActionValues('{$fileNames['deleteNotification']}', ['deleteNotif'], ['{$notification['nid']}'], ''); formAction('{$fileNames['viewNotifications']}', ['viewNotifications'], 'notificationsList')\" style=\"display: inline\"><input type=\"text\" id=\"deleteNotif\" value=\"{$notification['nid']}\" hidden><input type=\"text\" name=\"viewNotifications\" value=\"{$viewNotificationsButtonTxt}\" hidden=\"yes\"><input type=\"submit\" value=\"Delete\"></form></li>\n");
+				echo("\t\t\t<li><a href=\"http://{$notification['link']}\">{$notification['title']}</a> <span class=\"date\">" . date('d/m/y G:i', $notification['date']) . "</span><form action=\"javascript: formActionValues('{$fileNames['deleteNotification']}', ['deleteNotif'], ['{$notification['nid']}'], ''); formAction('{$fileNames['viewNotifications']}', ['viewNotifications'], 'notificationsList'); notificationLabel()\" style=\"display: inline\"><input type=\"text\" id=\"deleteNotif\" value=\"{$notification['nid']}\" hidden><input type=\"text\" name=\"viewNotifications\" value=\"{$viewNotificationsButtonTxt}\" hidden=\"yes\"><input type=\"submit\" value=\"Delete\"></form></li>\n");
 			}
-			echo ("<form action=\"javascript: formAction('{$fileNames['deleteAllNotifications']}', ['deleteAll'], ''); formAction('{$fileNames['viewNotifications']}', ['viewNotifications'], 'notificationsList')\" style=\"display: inline\"><input type=\"submit\" id=\"deleteAll\" value=\"Delete all\"></form>");
+			echo ("<form action=\"javascript: formAction('{$fileNames['deleteAllNotifications']}', ['deleteAll'], ''); formAction('{$fileNames['viewNotifications']}', ['viewNotifications'], 'notificationsList'); notificationLabel()\" style=\"display: inline\"><input type=\"submit\" id=\"deleteAll\" value=\"Delete all\"></form>");
 			echo("\t\t</ul>");
 		}
 	}
