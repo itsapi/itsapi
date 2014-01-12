05d47bba3cb38fc876ce8215120f57e1dcd2bcd9
Geraint White <geraintwhite@gmail.com>
2014-01-12 11:58:26 +0000

    Fixed placeholders

--- mysql.php
+++
@@ -1,8 +0,0 @@
-<?
-	function queryError($mysqli){
-		return 'MySQLi query failed: (' . mysqli_errno($mysqli) . ') ' . mysqli_error($mysqli);
-	}
-	$mysqli = mysqli_connect('localhost', '{itsapi_mysqlUser}', '{itsapi_mysqlPass}', '{itsapi_db}');
-	if (mysqli_connect_errno($mysqli)) {
-		printf("Connect failed: %s\n", mysqli_connect_error());
-	}
