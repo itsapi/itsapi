fc4f66208e64099bda85303385926673c92d33f4
Geraint White <geraint@android.net>
2014-03-01 21:49:26 +0000

    Fixed phpmailer path

--- include/funcs.php
+++ include/funcs.php
@@ -161,7 +161,7 @@
 	}
 	
 	function email($userData, $subject, $message) {
-		require_once('/var/www/phpmailer/class.phpmailer.php');
+		require_once('../../phpmailer/class.phpmailer.php');
 		
 		$mail = new PHPMailer();
 		
