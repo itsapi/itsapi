86b060d4563f408c6091d3d8b8c5e11f0c539c4f
Geraint White <geraint@android.net>
2014-03-01 21:31:34 +0000

    Fixed paths

--- include/funcs.php
+++ include/funcs.php
@@ -161,7 +161,7 @@
 	}
 	
 	function email($userData, $subject, $message) {
-		require_once('../phpmailer/class.phpmailer.php');
+		require_once($_SERVER['DOCUMENT_ROOT'].'/phpmailer/class.phpmailer.php');
 		
 		$mail = new PHPMailer();
 		
--- include/inc.php
+++ include/inc.php
@@ -1,7 +1,7 @@
 <?
 	error_reporting(E_ALL);
 	ini_set('display_errors', 1);
-	set_include_path('/var/www/itsapi');
+	set_include_path('/var/www/projects/itsapi');
 	include('include/conf.php');
 	include($fileNames['mysql']);
 	include($fileNames['funcs']);
