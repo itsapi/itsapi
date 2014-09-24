05325a7f0e14fc7a35c465d74cf563e20c3d80f0
Geraint White <geraint@android.net>
2014-09-24 20:11:57 +0100

    Updated google analytics script

--- include/inc.php
+++ include/inc.php
@@ -3,7 +3,7 @@
 	ini_set('display_errors', 1);
 	set_include_path('/var/www/projects/itsapi');
 	include('include/conf.php');
-    include('credentials.php');
+  include('credentials.php');
 	include($fileNames['mysql']);
 	include($fileNames['funcs']);
 	include($fileNames['login']);
--- js/google.js
+++ js/google.js
@@ -3,7 +3,5 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-38825205-2', 'itsapi.co.uk');
-ga('require', 'displayfeatures');
-ga('require', 'linkid', 'linkid.js');
+ga('create', 'UA-38825205-2', 'auto');
 ga('send', 'pageview');
